499bc09652112310a36a4a89db339dba2aa05d6f
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-01-25 19:47:01 +0700

    Contoh library sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BelajarPHP\Belajar;
+
+class Customer {
+
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, My name is $this->name";
+    }
+
+}
+
+?>
